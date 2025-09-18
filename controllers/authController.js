fc2cb23cf0d56e8ed9186f12720be8aed1688313
Suyash Tripathi <rajtr3142@gmail.com>
2025-09-18 17:46:03 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,7 +74,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // deploy pe true, localhost pe false
-      sameSite: "none", // cross-origin ke liye
+      sameSite: "None", // cross-origin ke liye
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
