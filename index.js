cc6fef83c8588866c2cc08d9c4a9b5a0c7fca13a
Suyash Tripathi <rajtr3142@gmail.com>
2025-09-18 17:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: process.env.FRONTEND_URL, // frontend URL
   credentials: true // <- must for cookies
 }));
 app.use(express.json());
